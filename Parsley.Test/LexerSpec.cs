85a646a5610a534ca73c7a3a6275931995d41519
Patrick Lioi <patrick.lioi@gmail.com>
2011-05-28 16:32:43 -0500

    Initial implementation of Lexer

---
+++ Parsley.Test/LexerSpec.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Parsley
+{
+    [TestFixture]
+    public sealed class LexerSpec
+    {
+        [Test]
+        public void ProvidesNullTokenAtEndOfInput()
+        {
+            var lexer = new Lexer(new Text(""));
+            lexer.CurrentToken.ShouldBeNull();
+        }
+
+        [Test]
+        public void TryingToAdvanceBeyondInputResultsInNoMovement()
+        {
+            var lexer = new Lexer(new Text(""));
+            lexer.ShouldBeTheSameAs(lexer.Advance());
+        }
+
+        [Test]
+        public void UsesPrioritizedTokenMatchersToGetCurrentToken()
+        {
+            var lexer = new Lexer(new Text("ABCdefGHI"), @"[a-z]+", @"[A-Z]+");
+            lexer.CurrentToken.ShouldEqual("ABC");
+            lexer.Advance().CurrentToken.ShouldEqual("def");
+            lexer.Advance().Advance().CurrentToken.ShouldEqual("GHI");
+            lexer.Advance().Advance().Advance().CurrentToken.ShouldBeNull();
+        }
+
+        [Test]
+        public void TreatsEntireRemainingTextAsOneTokenWhenAllMatchersFail()
+        {
+            var lexer = new Lexer(new Text("ABCdef!ABCdef"), @"[a-z]+", @"[A-Z]+");
+            lexer.CurrentToken.ShouldEqual("ABC");
+            lexer.Advance().CurrentToken.ShouldEqual("def");
+            lexer.Advance().Advance().CurrentToken.ShouldEqual("!ABCdef");
+            lexer.Advance().Advance().Advance().CurrentToken.ShouldBeNull();
+        }
+    }
+}
---
+++ Parsley/Lexer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Parsley
+{
+    public sealed class Lexer
+    {
+        private delegate string TokenMatcher(Text text);
+
+        private readonly Text text;
+        private readonly IEnumerable<TokenMatcher> matchers;
+
+        public Lexer(Text text, params string[] patterns)
+            : this(text, patterns.Select(Matcher))
+        {
+        }
+
+        private Lexer(Text text, IEnumerable<TokenMatcher> matchers)
+        {
+            this.text = text;
+            this.matchers = matchers;
+        }
+
+        public string CurrentToken
+        {
+            get
+            {
+                if (text.EndOfInput)
+                    return null;
+
+                foreach (var match in matchers)
+                {
+                    var token = match(text);
+                    if (token != null)
+                        return token;
+                }
+
+                string unknownToken = text.ToString();
+                return unknownToken;
+            }
+        }
+
+        public Lexer Advance()
+        {
+            if (text.EndOfInput)
+                return this;
+
+            return new Lexer(text.Advance(CurrentToken.Length), matchers);
+        }
+
+        private static TokenMatcher Matcher(string pattern)
+        {
+            return text =>
+            {
+                string peek = text.Peek(text.Count(pattern));
+
+                return peek == "" ? null : peek;
+            };
+        }
+    }
+}
