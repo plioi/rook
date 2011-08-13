ae14e43cb388130499b01f996ad7b9faeeadb2b2
Patrick Lioi <patrick.lioi@gmail.com>
2011-08-12 23:19:25 -0500

    Update Parsley to latest version.  Use new test helpers to simplify RookLexerTests

--- src/Rook.Test/Compiling/Syntax/RookLexerTests.cs
+++ src/Rook.Test/Compiling/Syntax/RookLexerTests.cs
@@ -9,41 +9,38 @@ namespace Rook.Compiling.Syntax
         [Test]
         public void ShouldRecognizeIntegers()
         {
-            AssertTokens("0", RookLexer.Integer, "0");
+            new RookLexer("0").ShouldYieldTokens(RookLexer.Integer, "0");
             
             //NOTE: Integer literals are not (yet) limited to int.MaxValue:
-            AssertTokens("2147483648", RookLexer.Integer, "2147483648");
+            new RookLexer("2147483648").ShouldYieldTokens(RookLexer.Integer, "2147483648");
         }
 
         [Test]
         public void ShouldRecognizeKeywords()
         {
-            AssertTokens("true", RookLexer.@true, "true");
-            AssertTokens("false", RookLexer.@false, "false");
-            AssertTokens("int", RookLexer.@int, "int");
-            AssertTokens("bool", RookLexer.@bool, "bool");
-            AssertTokens("void", RookLexer.@void, "void");
-            AssertTokens("null", RookLexer.@null, "null");
-            AssertTokens("if", RookLexer.@if, "if");
-            AssertTokens("else", RookLexer.@else, "else");
-            AssertTokens("fn", RookLexer.@fn, "fn");
+            new RookLexer("true").ShouldYieldTokens(RookLexer.@true, "true");
+            new RookLexer("false").ShouldYieldTokens(RookLexer.@false, "false");
+            new RookLexer("int").ShouldYieldTokens(RookLexer.@int, "int");
+            new RookLexer("bool").ShouldYieldTokens(RookLexer.@bool, "bool");
+            new RookLexer("void").ShouldYieldTokens(RookLexer.@void, "void");
+            new RookLexer("null").ShouldYieldTokens(RookLexer.@null, "null");
+            new RookLexer("if").ShouldYieldTokens(RookLexer.@if, "if");
+            new RookLexer("else").ShouldYieldTokens(RookLexer.@else, "else");
+            new RookLexer("fn").ShouldYieldTokens(RookLexer.@fn, "fn");
         }
 
         [Test]
         public void ShouldRecognizeIdentifiers()
         {
-            AssertTokens("a", RookLexer.Identifier, "a");
-            AssertTokens("ab", RookLexer.Identifier, "ab");
-            AssertTokens("a0", RookLexer.Identifier, "a0");
+            new RookLexer("a").ShouldYieldTokens(RookLexer.Identifier, "a");
+            new RookLexer("ab").ShouldYieldTokens(RookLexer.Identifier, "ab");
+            new RookLexer("a0").ShouldYieldTokens(RookLexer.Identifier, "a0");
         }
 
         [Test]
         public void ShouldRecognizeOperatorsGreedily()
         {
-            AssertTokens("<=>=<>!====*/+-&&||!{}[][,]()???:", "<=",
-                         ">=", "<", ">", "!=", "==", "=", "*", "/",
-                         "+", "-", "&&", "||", "!", "{", "}", "[]",
-                         "[", ",", "]", "(", ")", "??", "?", ":");
+            new RookLexer("<=>=<>!====*/+-&&||!{}[][,]()???:").ShouldYieldTokens("<=", ">=", "<", ">", "!=", "==", "=", "*", "/", "+", "-", "&&", "||", "!", "{", "}", "[]", "[", ",", "]", "(", ")", "??", "?", ":");
         }
 
         [Test]
@@ -51,46 +48,20 @@ namespace Rook.Compiling.Syntax
         {
             //Endlines are \r\n or semicolons (with optional preceding spaces/tabs and optional trailing whitspace).
 
-            AssertTokens("\r\n", RookLexer.EndOfLine, "\r\n");
-            AssertTokens("\r\n \r\n \t ", RookLexer.EndOfLine, "\r\n \r\n \t ");
+            new RookLexer("\r\n").ShouldYieldTokens(RookLexer.EndOfLine, "\r\n");
+            new RookLexer("\r\n \r\n \t ").ShouldYieldTokens(RookLexer.EndOfLine, "\r\n \r\n \t ");
 
-            AssertTokens(";", RookLexer.EndOfLine, ";");
-            AssertTokens("; \r\n \t ",RookLexer.EndOfLine, "; \r\n \t ");
+            new RookLexer(";").ShouldYieldTokens(RookLexer.EndOfLine, ";");
+            new RookLexer("; \r\n \t ").ShouldYieldTokens(RookLexer.EndOfLine, "; \r\n \t ");
         }
 
         [Test]
         public void ShouldRecognizeAndSkipOverIntralineWhitespace()
         {
-            AssertTokens(" a if == \r\n 0 ", "a", "if", "==", "\r\n ", "0");
-            AssertTokens("\ta\tif\t==\t\r\n\t0\t", "a", "if", "==", "\r\n\t", "0");
-            AssertTokens(" \t a \t if \t == \t \r\n \t 0 \t ", "a", "if", "==", "\r\n \t ", "0");
-            AssertTokens("\t \ta\t \tif\t \t==\t \t\r\n\t \t0\t \t", "a", "if", "==", "\r\n\t \t", "0");
-        }
-
-        private static void AssertTokens(string source, TokenKind expectedKind, params string[] expectedLiterals)
-        {
-            Lexer lexer = new RookLexer(source);
-
-            foreach (var expectedLiteral in expectedLiterals)
-            {
-                lexer.CurrentToken.ShouldBe(expectedKind, expectedLiteral);
-                lexer = lexer.Advance();
-            }
-
-            lexer.CurrentToken.Kind.ShouldEqual(Lexer.EndOfInput);
-        }
-
-        private static void AssertTokens(string source, params string[] expectedLiterals)
-        {
-            Lexer lexer = new RookLexer(source);
-
-            foreach (var expectedLiteral in expectedLiterals)
-            {
-                lexer.CurrentToken.Literal.ShouldEqual(expectedLiteral);
-                lexer = lexer.Advance();
-            }
-
-            lexer.CurrentToken.Kind.ShouldEqual(Lexer.EndOfInput);
+            new RookLexer(" a if == \r\n 0 ").ShouldYieldTokens("a", "if", "==", "\r\n ", "0");
+            new RookLexer("\ta\tif\t==\t\r\n\t0\t").ShouldYieldTokens("a", "if", "==", "\r\n\t", "0");
+            new RookLexer(" \t a \t if \t == \t \r\n \t 0 \t ").ShouldYieldTokens("a", "if", "==", "\r\n \t ", "0");
+            new RookLexer("\t \ta\t \tif\t \t==\t \t\r\n\t \t0\t \t").ShouldYieldTokens("a", "if", "==", "\r\n\t \t", "0");
         }
     }
 }
