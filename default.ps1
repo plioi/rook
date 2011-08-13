900f5326fca62a66d1e3f08b6456f72daa72b080
Patrick Lioi <patrick.lioi@gmail.com>
2011-08-12 22:39:11 -0500

    Simplify build script.

--- default.ps1
+++ default.ps1
@@ -1,17 +1,16 @@
 properties {
     $projectName = "Rook"
-    $unitTestAssembly = "Rook.Test.dll"
 
     $projectConfig = "Release"
     $base_dir = resolve-path .\
     $source_dir = "$base_dir\src"
-    $samples_dir = "$source_dir\Rook.Test\Samples"
-    $nunit_dir = "$source_dir\packages\NUnit.2.5.10.11092\Tools"
 	
     $build_dir = "$base_dir\build"
-    $test_dir = "$build_dir\test"
+    $unittest_dir = "$base_dir\src\Rook.Test\bin\$projectConfig"
     $package_dir = "$build_dir\package"	
-    $package_file = "$build_dir\latestVersion\" + $projectName +".zip"
+    $package_file = "$build_dir\" + $projectName +".zip"
+
+    $defaultVersion = "1.0.0.0"
 }
 
 $framework = '4.0'
@@ -20,42 +19,40 @@ FormatTaskName "------------------------------
 -- {0}
 ------------------------------"
 
-task default -depends Init, CommonAssemblyInfo, Compile, Test
-task package -depends Init, CommonAssemblyInfo, Compile, Test, ZipPackage
+task default -depends Compile, Test, Package
 
 task Init {
-    delete_file $package_file
     delete_directory $build_dir
-    create_directory $test_dir
     create_directory $build_dir
 }
 
 task CommonAssemblyInfo {
-    $version = "1.0.0.0"
+    if(-not $version)
+    {
+        $version = $defaultVersion
+    }
     create-commonAssemblyInfo "$version" $projectName "$source_dir\CommonAssemblyInfo.cs"
 }
 
-task Compile -depends Init {
+task Compile -depends Init, CommonAssemblyInfo {
     msbuild /t:clean /v:q /nologo /p:Configuration=$projectConfig $source_dir\$projectName.sln
-    delete_file $error_dir
     msbuild /t:build /v:q /nologo /p:Configuration=$projectConfig $source_dir\$projectName.sln
 }
 
-task Test {
-    copy_all_assemblies_for_test $test_dir
-    copy_samples_for_test $test_dir
+task Test -depends Compile {
     exec {
-        & $nunit_dir\nunit-console.exe $test_dir\$unitTestAssembly /nologo /nodots /xml=$build_dir\TestResult.xml    
+        & $base_dir\src\packages\NUnit.2.5.10.11092\tools\nunit-console.exe $unittest_dir\$projectName.Test.dll /nologo /nodots /xml=$build_dir\TestResult.xml    
     }
 }
 
-task ZipPackage -depends Compile {
+task Package -depends Compile {
     delete_directory $package_dir
 
     copy_files "$source_dir\Rook\bin\$projectConfig\" $package_dir
     Copy-Item "$base_dir\README.md" "$package_dir\README.txt"
 
     zip_directory $package_dir $package_file 
+    write-host "Created deployment package: $package_file" -ForegroundColor Green
 }
 
 function global:zip_directory($directory,$file) {
@@ -68,24 +65,6 @@ function global:zip_directory($directory,$file) {
 function global:copy_files($source,$destination,$exclude=@()){    
     create_directory $destination
     Get-ChildItem $source -Recurse -Exclude $exclude | Copy-Item -Destination {Join-Path $destination $_.FullName.Substring($source.length)} 
-}
-
-function global:copy_and_flatten ($source,$filter,$dest) {
-    ls $source -filter $filter -r | cp -dest $dest
-}
-
-function global:copy_all_assemblies_for_test($destination){
-    create_directory $destination
-    copy_and_flatten $source_dir *.exe $destination
-    copy_and_flatten $source_dir *.dll $destination
-    copy_and_flatten $source_dir *.config $destination
-    copy_and_flatten $source_dir *.xml $destination
-    copy_and_flatten $source_dir *.pdb $destination
-}
-
-function global:copy_samples_for_test($destination) {
-    create_directory "$destination\Samples"
-    copy_files $samples_dir "$destination\Samples"
 }
 
 function global:delete_file($file) {
