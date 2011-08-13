e14d5959933313be2e2b139cbbf3a683d1771895
Patrick Lioi <patrick.lioi@gmail.com>
2011-08-13 17:43:24 -0500

    Reorder and group grammar rules for readability.

--- src/Rook.Compiling/Syntax/RookGrammar.cs
+++ src/Rook.Compiling/Syntax/RookGrammar.cs
@@ -16,6 +16,7 @@ namespace Rook.Compiling.Syntax
         public static readonly GrammarRule<Token> Identifier = new GrammarRule<Token>();
         public static readonly GrammarRule<Name> Name = new GrammarRule<Name>();
 
+        #region Private Rules
         private static readonly GrammarRule<NamedType> NameType = new GrammarRule<NamedType>();
         private static readonly GrammarRule<Token> TypeModifier = new GrammarRule<Token>();
         private static readonly GrammarRule<NamedType> KeywordType = new GrammarRule<NamedType>();
@@ -47,8 +48,23 @@ namespace Rook.Compiling.Syntax
         private static readonly GrammarRule<Expression> BooleanLiteral = new GrammarRule<Expression>();
         private static readonly GrammarRule<Expression> IntegerLiteral = new GrammarRule<Expression>();
         private static readonly GrammarRule<Expression> NullLiteral = new GrammarRule<Expression>();
+        #endregion
 
         static RookGrammar()
+        {
+            TopLevelConstructs();
+            TypeNames();
+            Parameters();
+            Expressions();
+
+            EndOfLine.Rule =
+                Label(Choice(Token(RookLexer.EndOfLine), Token(Lexer.EndOfInput)), "end of line");
+
+            Identifier.Rule =
+                Token(RookLexer.Identifier);
+        }
+
+        private static void TopLevelConstructs()
         {
             Program.Rule =
                 from leadingEndOfLine in Optional(Token(RookLexer.EndOfLine))
@@ -61,7 +77,10 @@ namespace Rook.Compiling.Syntax
                 from parameters in Tuple(Parameter).TerminatedBy(Optional(EndOfLine))
                 from body in Expression
                 select new Function(name.Position, returnType, name, parameters, body);
+        }
 
+        private static void TypeNames()
+        {
             NameType.Rule =
                 from name in Name
                 select new NamedType(name.Identifier);
@@ -79,7 +98,10 @@ namespace Rook.Compiling.Syntax
                       from modifiers in ZeroOrMore(TypeModifier)
                       select modifiers.Aggregate(rootType, ApplyTypeModifier),
                       "type name");
+        }
 
+        private static void Parameters()
+        {
             ExplicitlyTypedParameter.Rule =
                 from type in TypeName
                 from identifier in Identifier
@@ -91,7 +113,10 @@ namespace Rook.Compiling.Syntax
 
             Parameter.Rule =
                 Choice(Attempt(ExplicitlyTypedParameter), ImplicitlyTypedParameter);
+        }
 
+        private static void Expressions()
+        {
             Expression.Rule = NullCoalescing;
             NullCoalescing.Rule = Binary(Or, "??");
             Or.Rule = Binary(And, "||");
@@ -106,17 +131,6 @@ namespace Rook.Compiling.Syntax
                 from primary in Primary
                 select symbol == null ? primary : new Call(symbol.Position, symbol.Literal, primary);
 
-            Call.Rule =
-                from arguments in Tuple(Expression)
-                select new Func<Expression, Expression>(callable => new Call(callable.Position, callable, arguments));
-
-            Subscript.Rule =
-                from arguments in Between("[", OneOrMore(Expression, Token(":")), "]")
-                select new Func<Expression, Expression>(
-                    callable => new Call(callable.Position,
-                                         new Name(callable.Position, arguments.Count() == 1 ? "Index" : "Slice"),
-                                         new[] {callable}.Concat(arguments)));
-
             var nothing = from zeroArguments in Zero<Expression>()
                           select new Func<Expression, Expression>(callable => callable);
 
@@ -127,6 +141,22 @@ namespace Rook.Compiling.Syntax
 
             Literal.Rule =
                 Choice(BooleanLiteral, NullLiteral, IntegerLiteral);
+
+            BooleanLiteral.Rule =
+                from token in Choice(Token(RookLexer.@true), Token(RookLexer.@false))
+                select new BooleanLiteral(token.Position, bool.Parse(token.Literal));
+
+            IntegerLiteral.Rule =
+                from token in Token(RookLexer.Integer)
+                select new IntegerLiteral(token.Position, token.Literal);
+
+            NullLiteral.Rule =
+                from literal in Token(RookLexer.@null)
+                select new Null(literal.Position);
+
+            VectorLiteral.Rule =
+                from items in Between("[", OneOrMore(Expression, Token(",")), "]")
+                select new VectorLiteral(items.First().Position, items);
 
             If.Rule =
                 from _if_ in Token(RookLexer.@if)
@@ -149,9 +179,23 @@ namespace Rook.Compiling.Syntax
                 from body in Expression
                 select new Lambda(_fn_.Position, parameters, body);
 
-            VectorLiteral.Rule =
-                from items in Between("[", OneOrMore(Expression, Token(",")), "]")
-                select new VectorLiteral(items.First().Position, items);
+            Name.Rule =
+                from identifier in Identifier
+                select new Name(identifier.Position, identifier.Literal);
+
+            Call.Rule =
+                from arguments in Tuple(Expression)
+                select new Func<Expression, Expression>(callable => new Call(callable.Position, callable, arguments));
+
+            Subscript.Rule =
+                from arguments in Between("[", OneOrMore(Expression, Token(":")), "]")
+                select new Func<Expression, Expression>(
+                    callable => new Call(callable.Position,
+                                         new Name(callable.Position, arguments.Count() == 1 ? "Index" : "Slice"),
+                                         new[] {callable}.Concat(arguments)));
+
+            VariableDeclaration.Rule =
+                Choice(Attempt(ExplicitlyTypedVariableDeclaration), ImplicitlyTypedVariableDeclaration);
 
             ExplicitlyTypedVariableDeclaration.Rule =
                 from type in TypeName
@@ -167,34 +211,8 @@ namespace Rook.Compiling.Syntax
                 from value in Expression
                 from end in EndOfLine
                 select new VariableDeclaration(identifier.Position, identifier.Literal, value);
-
-            VariableDeclaration.Rule =
-                Choice(Attempt(ExplicitlyTypedVariableDeclaration), ImplicitlyTypedVariableDeclaration);
-
-            BooleanLiteral.Rule =
-                from token in Choice(Token(RookLexer.@true), Token(RookLexer.@false))
-                select new BooleanLiteral(token.Position, bool.Parse(token.Literal));
-
-            IntegerLiteral.Rule =
-                from token in Token(RookLexer.Integer)
-                select new IntegerLiteral(token.Position, token.Literal);
-
-            NullLiteral.Rule =
-                from literal in Token(RookLexer.@null)
-                select new Null(literal.Position);
-
-            EndOfLine.Rule =
-                Label(Choice(Token(RookLexer.EndOfLine), Token(Lexer.EndOfInput)), "end of line");
-
-            Identifier.Rule =
-                Token(RookLexer.Identifier);
-
-            Name.Rule =
-                from identifier in Identifier
-                select new Name(identifier.Position, identifier.Literal);
         }
 
-        #region Helpers
         private static Parser<T> Between<T>(string openOperator, Parser<T> parser, string closeOperator)
         {
             return Between(Token(openOperator), parser, Token(closeOperator));
@@ -230,6 +248,5 @@ namespace Rook.Compiling.Syntax
                                    new Call(symbolAndRight.Item1.Position, symbolAndRight.Item1.Literal, left,
                                             symbolAndRight.Item2));
         }
-        #endregion
     }
 }
