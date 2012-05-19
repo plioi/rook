a1492d47ef259219a79ecbfd710beb9bf647ca0d
Patrick Lioi <patrick.lioi@gmail.com>
2012-05-19 11:31:54 -0500

    First pass at type checking New syntax nodes.  The type of a constructor call is the type being constructed.

--- src/Rook.Compiling/Syntax/New.cs
+++ src/Rook.Compiling/Syntax/New.cs
@@ -7,21 +7,34 @@ namespace Rook.Compiling.Syntax
     {
         public Position Position { get; private set; }
         public Name TypeName { get; private set; }
+        public DataType Type { get; private set; }
 
         public New(Position position, Name typeName)
+            :this(position, typeName, null) { }
+
+        public New(Position position, Name typeName, DataType type)
         {
             Position = position;
             TypeName = typeName;
-        }
-
-        public DataType Type
-        {
-            get { throw new System.NotImplementedException(); }
+            Type = type;
         }
 
         public TypeChecked<Expression> WithTypes(Environment environment)
         {
-            throw new System.NotImplementedException();
+            var typeCheckedTypeName = TypeName.WithTypes(environment);
+
+            if (typeCheckedTypeName.HasErrors)
+                return typeCheckedTypeName;
+
+            //TODO: It isn't enough to just have a known identifier.
+            //      The identifier should be the name of a known class.
+            //      See how Call type checks by expecting a Func type.
+            //      New should check by expecting a class type.
+            //      Consider changing the meaning of "the type of a class decaration".
+
+            var typedTypeName = (Name)typeCheckedTypeName.Syntax;
+
+            return TypeChecked<Expression>.Success(new New(Position, typedTypeName, new NamedType(TypeName.Identifier)));
         }
 
         public TResult Visit<TResult>(Visitor<TResult> visitor)
--- src/Rook.Test/Compiling/Syntax/NewTests.cs
+++ src/Rook.Test/Compiling/Syntax/NewTests.cs
@@ -1,4 +1,6 @@
 using Parsley;
+using Rook.Compiling.Types;
+using Should;
 using Xunit;
 
 namespace Rook.Compiling.Syntax
@@ -6,12 +8,39 @@ namespace Rook.Compiling.Syntax
     public class NewTests : ExpressionTests
     {
         [Fact]
-        public void ConstructorCalls()
+        public void InvokesConstructorByTypeName()
         {
             FailsToParse("new").AtEndOfInput().WithMessage("(1, 4): identifier expected");
             FailsToParse("new Foo").AtEndOfInput().WithMessage("(1, 8): ( expected");
             FailsToParse("new Foo(").AtEndOfInput().WithMessage("(1, 9): ) expected");
             Parses("new Foo()").IntoTree("(new Foo())");
+        }
+
+        [Fact]
+        public void FailsTypeCheckingForTypeNameNotInScope()
+        {
+            TypeChecking("new Foo()").ShouldFail("Reference to undefined identifier: Foo", 1, 5);
+        }
+
+        [Fact]
+        public void HasATypeEqualToThatOfTheTypeBeingConstructed()
+        {
+            var type = new NamedType("Foo");
+            Type("new Foo()", Foo => type).ShouldEqual(type);
+        }
+
+        [Fact]
+        public void CanCreateFullyTypedInstance()
+        {
+            var type = new NamedType("Foo");
+
+            var @new = (New)Parse("new Foo()");
+            @new.Type.ShouldBeNull();
+            @new.TypeName.Type.ShouldBeNull();
+
+            var typedNew = (New)@new.WithTypes(Environment(Foo => type)).Syntax;
+            typedNew.Type.ShouldEqual(type);
+            typedNew.TypeName.Type.ShouldEqual(type);
         }
     }
 }
