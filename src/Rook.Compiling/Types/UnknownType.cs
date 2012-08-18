52686db2b801f414c932f92e390f44ab68c7040b
Patrick Lioi <patrick.lioi@gmail.com>
2012-08-18 13:26:53 -0500

    Add UnknownType.

---
+++ src/Rook.Compiling/Types/UnknownType.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rook.Compiling.Types
+{
+    public class UnknownType : DataType
+    {
+        public static readonly UnknownType Instance = new UnknownType();
+
+        private UnknownType() { }
+
+        public override string Name
+        {
+            get { return "?"; }
+        }
+
+        public override IEnumerable<DataType> InnerTypes
+        {
+            get { return Enumerable.Empty<DataType>(); }
+        }
+
+        public override bool Contains(TypeVariable typeVariable)
+        {
+            return false;
+        }
+
+        public override IEnumerable<TypeVariable> FindTypeVariables()
+        {
+            return Enumerable.Empty<TypeVariable>();
+        }
+
+        public override DataType ReplaceTypeVariables(IDictionary<TypeVariable, DataType> substitutions)
+        {
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/Rook.Test/Compiling/Types/UnknownTypeTests.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Should;
+using Xunit;
+
+namespace Rook.Compiling.Types
+{
+    public class UnknownTypeTests
+    {
+        private static readonly UnknownType Unknown = UnknownType.Instance;
+
+        [Fact]
+        public void HasAName()
+        {
+            Unknown.Name.ShouldEqual("?");
+        }
+
+        [Fact]
+        public void HasAStringRepresentation()
+        {
+            Unknown.ToString().ShouldEqual("?");
+        }
+
+        [Fact]
+        public void HasZeroInnerTypes()
+        {
+            Unknown.InnerTypes.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void DoesNotContainTypeVariables()
+        {
+            Unknown.Contains(new TypeVariable(0)).ShouldBeFalse();
+            Unknown.FindTypeVariables().ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void TypeVariableSubstitutionIsAnIdentityOperation()
+        {
+            var substitutions = new Dictionary<TypeVariable, DataType> { { new TypeVariable(0), NamedType.Integer } };
+
+            Unknown.ReplaceTypeVariables(substitutions).ShouldEqual(Unknown);
+        }
+
+        [Fact]
+        public void HasValueEqualitySemantics()
+        {
+            Unknown.ShouldEqual(Unknown);
+            Unknown.ShouldNotEqual((DataType)new TypeVariable(0));
+            Unknown.ShouldNotEqual((DataType)NamedType.Integer);
+            Unknown.GetHashCode().ShouldNotEqual(NamedType.Integer.GetHashCode());
+        }
+    }
+}
