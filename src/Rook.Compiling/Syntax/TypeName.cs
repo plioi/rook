72066f5b436b0347b9fb8866cbe684676f457458
Patrick Lioi <patrick.lioi@gmail.com>
2012-09-14 20:27:24 -0500

    TypeName is a light-weight value type representing anything you could type inside of the C# typeof(...) operator.

---
+++ src/Rook.Compiling/Syntax/TypeName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rook.Core;
+
+namespace Rook.Compiling.Syntax
+{
+    public class TypeName : Value<TypeName>
+    {
+        private readonly string name;
+        private readonly TypeName[] genericArguments;
+        private readonly string fullName;
+
+        public TypeName(string name, params TypeName[] genericArguments)
+        {
+            this.name = name;
+            this.genericArguments = genericArguments;
+
+            fullName = genericArguments.Any()
+                           ? String.Format("{0}<{1}>", name, String.Join(", ", (IEnumerable<TypeName>) genericArguments))
+                           : name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public IEnumerable<TypeName> GenericArguments
+        {
+            get { return genericArguments; }
+        }
+
+        protected override object[] ImmutableFields()
+        {
+            return new object[] { fullName };
+        }
+
+        public override string ToString()
+        {
+            return fullName;
+        }
+    }
+}
---
+++ src/Rook.Test/Compiling/Syntax/TypeNameTests.cs
@@ -0,0 +1,39 @@
+﻿using Should;
+
+namespace Rook.Compiling.Syntax
+{
+    [Facts]
+    public class TypeNameTests
+    {
+        public void HasAName()
+        {
+            new TypeName("A").Name.ShouldEqual("A");
+            new TypeName("B", new TypeName("A")).Name.ShouldEqual("B");
+        }
+
+        public void HasZeroOrMoreGenericArguments()
+        {
+            new TypeName("A").GenericArguments.ShouldBeEmpty();
+            new TypeName("B", new TypeName("A")).GenericArguments.ShouldList(new TypeName("A"));
+            new TypeName("C", new TypeName("B", new TypeName("A"))).GenericArguments.ShouldList(new TypeName("B", new TypeName("A")));
+        }
+
+        public void HasAStringRepresentation()
+        {
+            new TypeName("A").ToString().ShouldEqual("A");
+            new TypeName("A", new TypeName("B")).ToString().ShouldEqual("A<B>");
+            new TypeName("A", new TypeName("B", new TypeName("C"), new TypeName("D"))).ToString().ShouldEqual("A<B<C, D>>");
+        }
+
+        public void HasValueEqualitySemantics()
+        {
+            var type = new TypeName("B", new TypeName("A"));
+            type.ShouldEqual(type);
+            type.ShouldEqual(new TypeName("B", new TypeName("A")));
+            type.ShouldNotEqual(new TypeName("B"));
+
+            type.GetHashCode().ShouldEqual(new TypeName("B", new TypeName("A")).GetHashCode());
+            type.GetHashCode().ShouldNotEqual(new TypeName("B").GetHashCode());
+        }
+    }
+}
